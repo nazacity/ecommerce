60dfe22d21498c3b4b65e32894daecd27c969906
nazacity <warodom52@gmail.com>
2020-02-05 08:49:12 +0700

    Updates

---
+++ precache-manifest.72c7899a18625aa221e3b0d9243994de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41a1acf3e9e0ca7c9082612bf278178",
+    "url": "/nazacity/ecommerce.git/index.html"
+  },
+  {
+    "revision": "59218d4dd3e6eeee2df5",
+    "url": "/nazacity/ecommerce.git/static/css/main.54ce862a.chunk.css"
+  },
+  {
+    "revision": "011e3d0467ed25ddf254",
+    "url": "/nazacity/ecommerce.git/static/js/2.b31df28d.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/nazacity/ecommerce.git/static/js/2.b31df28d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59218d4dd3e6eeee2df5",
+    "url": "/nazacity/ecommerce.git/static/js/main.4c319c23.chunk.js"
+  },
+  {
+    "revision": "2a9123d54759994fc608",
+    "url": "/nazacity/ecommerce.git/static/js/runtime-main.1a9c2b31.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/nazacity/ecommerce.git/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/nazacity/ecommerce.git/static/media/shopping-bag.30f07071.svg"
+  }
+]);
